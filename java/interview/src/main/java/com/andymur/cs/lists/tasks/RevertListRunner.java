e29fb2b3d9449c5148f7c659652b4f80bf5734b2
andymur <andymurh@gmail.com>
2018-10-16 22:00:58 +0200

    Add reverse list method

--- java/interview/src/main/java/com/andymur/cs/lists/tasks/RevertListRunner.java
+++ java/interview/src/main/java/com/andymur/cs/lists/tasks/RevertListRunner.java
@@ -2,9 +2,12 @@ package com.andymur.cs.lists.tasks;
 
 
 public class RevertListRunner {
+
     public static void main(String[] args) {
         List<String> abc = constructFromString("ABC");
+        List<String> cba = reverseList(abc);
         abc.print();
+        cba.print();
     }
 
     public static List<String> constructFromString(final String content) {
@@ -17,10 +20,53 @@ public class RevertListRunner {
         return result;
     }
 
+    public static <E> List<E> reverseList(List<E> originalList) {
+        final List<E> result = new List<>();
+
+        if (originalList == null) {
+            return null;
+        }
+
+        if (originalList.getHead() == null) {
+            return result;
+        }
+
+        if (originalList.getHead().next() == null) {
+            result.append(originalList.getHead().element());
+            return result;
+        }
+
+        // NULL (p) o1(c) -> o2(n) -> NULL
+        // NULL <- o1(p) o2(c) -> NULL(n)
+        // NULL <- o1 <- o2 NULL
+
+        Node<E> prev = null;
+        Node<E> current = originalList.getHead();
+        Node<E> nxt = current.next();
+
+        while (nxt != null) {
+            current.setNext(prev);
+            prev = current;
+            current = nxt;
+            nxt = current.next();
+        }
+
+        current.setNext(prev);
+
+        return new List<>(current);
+    }
+
     public static class List<E> {
 
         private Node<E> head = null;
         private Node<E> tail = null;
+
+        public List() {
+        }
+
+        public List(Node<E> head) {
+            this.head = head;
+        }
 
         public E append(E element) {
             assert tail == null || tail.next() == null;

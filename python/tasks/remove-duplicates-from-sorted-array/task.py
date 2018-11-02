65b586c3ca69a96774d7aea410bd4e83900976b5
andymur <andymurh@gmail.com>
2018-11-02 14:04:45 +0100

    a

---
+++ python/tasks/remove-duplicates-from-sorted-array/task.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+'''Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length. 
+Do not allocate extra space for another array, you must do this in place with constant memory.
+For example, given input array A = [1,1,2], your function should return length = 2, and A is now [1,2].'''
+
+
+if __name__ == "__main__":
+	a = [1, 1, 2]	

34175b1897519020ec3a0330e52a8cfda726be60
andymur <andymurh@gmail.com>
2018-11-21 09:50:43 +0100

    twitter task conditions

---
+++ python/tasks/twitter-water-overflow/task.py
@@ -0,0 +1,15 @@
+
+'''
+There is an array of positive integer which represents height of the bloks of the wall.
+Compute number of block filled with water
+
+e.g. this wall could be represented by array of height [3, 1, 3]
+
+[]  [] 
+[]  []
+[][][]
+3 1 3
+
+and the answer is 2 cause two blocks will be filled with water in case of eternal rain
+
+'''

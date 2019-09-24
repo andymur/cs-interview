d3d3d694a14bde9cdcc7a468333ca6aa5f202e19
andymur <andymurh@gmail.com>
2019-09-24 20:31:17 +0200

    add smallest window string task

--- python/tasks/smallest-window-string/task.py
+++ python/tasks/smallest-window-string/task.py
@@ -1,5 +1,36 @@
 #!/usr/bin/python3
 
 
+'''https://www.geeksforgeeks.org/find-the-smallest-window-in-a-string-containing-all-characters-of-another-string/
+
+Find the smallest window in a string containing all characters of another string
+Given two strings string1 and string2, find the smallest substring in string1 containing all characters of string2 efficiently.
+
+Input :  string = "this is a test string"
+         pattern = "tist"
+Output :  Minimum window is "t stri"
+Explanation: "t stri" contains all the characters
+              of pattern.
+
+Input :  string = "geeksforgeeks"
+         pattern = "ork" 
+Output :  Minimum window is "ksfor"
+'''
+
+def check_window(window, pattern):
+	
+
+def window_by_size(s_input, window_size):
+	input_len = len(s_input)
+
+	if window_size > input_len:
+		return
+
+
+
 if __name__ == "__main__":
-    print("A")
+	s_input = "geeksforgeeks"
+	pattern = "ork"
+
+	pattern_len = len(pattern)
+
